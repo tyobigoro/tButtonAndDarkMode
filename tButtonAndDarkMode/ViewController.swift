2739f08553b8b1291847901fbb97b350b014acb7
tyobigoro <n_nakano@mbm.nifty.com>
2019-11-05 08:55:53 +0900

    c

--- tButtonAndDarkMode/ViewController.swift
+++ tButtonAndDarkMode/ViewController.swift
@@ -11,16 +11,29 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var tBtn: SampltBtn!
+    @IBOutlet weak var tBtn: ButtonWithDotmark!
+    
+    @IBOutlet weak var basicBtn: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func btnDidTap(_ sender: Any) {
+    @IBAction func isEnabledBtnDidTap(_ sender: Any) {
+        tBtn.isEnabled.toggle()
+    }
+    
+    
+    @IBAction func isLPEnabledBtnDidTap(_ sender: Any) {
         tBtn.isLongPressEnabled.toggle()
     }
+    
+    @IBAction func isEnabledBtn_sample(_ sender: Any) {
+        basicBtn.isEnabled.toggle()
+    }
+    
     
 }
 
@@ -76,18 +89,23 @@ class ViewController: UIViewController {
 }
 
 @IBDesignable
-class ButtonWithUnderBar: UIButton {
+class ButtonWithDotmark: UIButton {
   
     override var isEnabled: Bool {
         didSet {
             super.isEnabled = isEnabled
             if oldValue != isEnabled {
-                underBarVisibleStatus = isEnabled
-                underBarColor = self.isEnabled ? #colorLiteral(red: 0.4756349325, green: 0.4756467342, blue: 0.4756404161, alpha: 1) : #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
-                self.setNeedsDisplay()
+                setColorOfMark()
             }
         }
     }
+    
+    
+    var isLongPressEnabled: Bool = false {
+        didSet {
+            if oldValue != isLongPressEnabled {
+                setColorOfMark()
+    }}}
 
     private var customBackgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     
@@ -97,23 +115,15 @@ class ButtonWithUnderBar: UIButton {
             super.backgroundColor = UIColor.clear
     }}
     
-    let underBarWidth: CGFloat = 45.0
     
-    var underBarVisibleStatus: Bool = true {
-        didSet {
-            if oldValue != underBarVisibleStatus {
-                self.setNeedsDisplay()
-    }}}
+    var size: CGFloat = 6
     
-    var underBarColor: UIColor = #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 1) {
-        didSet {
-            if oldValue != underBarColor {
-                self.setNeedsDisplay()
-    }}}
+    var color: CGColor = UIColor.lightGray.cgColor
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.setup()
+        //self.setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -121,12 +131,14 @@ class ButtonWithUnderBar: UIButton {
         self.setup()
     }
     
+    
+    
     func setup() {
-        layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset.width = 0.0
-        layer.shadowOffset.height = 1.0
-        layer.shadowRadius = 5.0
-        layer.shadowOpacity = 0.2
+        layer.shadowColor = self.layer.shadowColor
+        layer.shadowOffset.width = self.layer.shadowOffset.width
+        layer.shadowOffset.height = self.layer.shadowOffset.height
+        layer.shadowRadius = self.layer.shadowRadius
+        layer.shadowOpacity = self.layer.shadowOpacity
         super.backgroundColor = UIColor.clear
     }
     
@@ -141,20 +153,43 @@ class ButtonWithUnderBar: UIButton {
         borderPath.lineWidth = borderWidth
         borderPath.stroke()
         
-        if underBarVisibleStatus {
-            let underBar = UIBezierPath()
-            let underBarWidth = self.frame.width / 3
-            underBar.move(
-                to: CGPoint(x: self.bounds.minX + underBarWidth, y: self.bounds.maxY - 4))
-            underBar.addLine(
-                to: CGPoint(x: self.bounds.maxX - underBarWidth, y: self.bounds.maxY - 4))
-            underBar.lineJoinStyle = .round
-            underBar.close()
-            underBar.lineWidth = 2.5
-            underBarColor.setStroke()
-            underBar.stroke()
-        }
+        drawDotMark()
+    }
+    
+    func drawDotMark() {
+        
+        let w: CGFloat = size
+        let h: CGFloat = size
+        
+        let x: CGFloat = self.frame.width - w * 2.5
+        let y: CGFloat = self.frame.height - h * 2.5
+        
+        let markLayer = CAShapeLayer.init()
+        let markFrame = CGRect(x: x, y: y, width: w, height: h)
+        markLayer.frame = markFrame
+        
+        markLayer.strokeColor = color
+        markLayer.fillColor = color
+        markLayer.lineWidth = 0.5
+        
+        markLayer.path = UIBezierPath.init(ovalIn: CGRect.init(x: 0,
+                                                               y: 0,
+                                                               width: markFrame.size.width,
+                                                               height: markFrame.size.height)).cgPath
+        
+        self.layer.addSublayer(markLayer)
         
     }
+    
+    func setColorOfMark() {
+        if isEnabled {
+            color = isLongPressEnabled ? UIColor.darkGray.cgColor : UIColor.lightGray.cgColor
+        } else {
+            color = UIColor.lightGray.cgColor
+        }
+        self.setNeedsDisplay()
+    }
+    
+ 
     
 }
